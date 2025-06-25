aeaef057eb6d6431f1356613cd25be7a5451bf93
Leo <FABISHAN2024@gmail.com>
2025-06-25 16:40:53 +0800

    Finished the game for 1 word

--- Hangman/Hangman.py
+++ Hangman/Hangman.py
@@ -10,11 +10,8 @@ for i in wordToGuess:
     wordBreakdown.append(i)
     playerGuesses.append("_")
 
-# print (wordBreakdown)
-# print (playerGuesses.count("_"))
-
-while gameRunning:
-    
+# print (wordBreakdown.)
+# print (wordToGuess.find("a"))
 
     print ("Welcome to Hangman!")
     print ("This is a game where you guess a word by guessing individual letters.")
@@ -23,7 +20,37 @@ while gameRunning:
     print (f"You have {playerLives} lives, and this word has {playerGuesses.count("_")} letters!")
     print ("Good Luck!")
 
-    letterToGuess = input ("Please guess your a letter")
+while gameRunning:
+    # gameRunning = False
 
-    if playerLives == 0 or playerGuesses.count("_") == 0:
+
+
+    print (f"Here is the word to guess: {playerGuesses}")
+
+    letterToGuess = input ("Please guess your a letter \n")
+
+    # if wordToGuess.count("letterToGuess", 0, len(wordToGuess)) > 0:
+    #     print ("Letter Found")
+    #     wordToGuess.find(letterToGuess)
+    guessOccurance = 0
+    for i in range(len(playerGuesses)):
+        if wordBreakdown[i] == letterToGuess:
+            playerGuesses[i] = letterToGuess
+            guessOccurance += 1
+
+    if guessOccurance == 0:
+        playerLives -= 1
+
+    print (f"You guessed the letter {letterToGuess}. it appeared {guessOccurance} times in the word.")
+    print (f"Here are your guesses {playerGuesses}")
+    print (f"You have {playerLives} left.")
+
+    if playerLives == 0:
         gameRunning = False
+        print ("You have lost all lives.")
+        print ("The game is over!")
+    
+    if playerGuesses.count("_") == 0:
+        gameRunning = False
+        print("You have correctly guessed the word!")
+        print ("The game is over!")
